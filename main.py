65653430037ba005e37f9f96b70c101090e9d7a9
Zakharov Oleg <ozakharov@mangistau.local>
2016-05-31 12:50:46 +0500

    Working prototype

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from telegram.error import NetworkError, Unauthorized
 import logging
 
 
-def getKKB():
+def get_kkb():
     html = urlopen('http://kkb.kz')
     bsHtml = BeautifulSoup(html.read(),'lxml')
     currencyList = bsHtml.find('table', {'class':'tbl_kurs'}).findAll('td')
@@ -14,18 +14,23 @@ def getKKB():
         currencyStripList.append(currency.get_text())
     return currencyStripList
 
+
+def kkb(bot, update):
+    a = get_kkb()
+    bot.sendMessage(update.message.chat_id, text='Kazkom'+'\n' + 'Валюта ' + ' '.join(a[2:4]))
+    bot.sendMessage(update.message.chat_id, text=' '.join(a[4:7])+'\n' + ' '.join(a[7:10])+'\n' + ' '.join(a[10:]))
+
+
 def main():
-    TOKEN = []
+    TOKEN = "197814627:AAEJ1fYVhxaa6LoUY78BoTA_jAuaqnuOlzY"
     updater = Updater(TOKEN)
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
 
     # Add handlers for Telegram messages
-    dp.addTelegramCommandHandler("help", help)
-    dp.addTelegramCommandHandler("addcard", add_card)
-    dp.addTelegramCommandHandler("removecard", remove_card)
-    dp.addTelegramCommandHandler("getcards", get_cards)
+    dp.add_handler(CommandHandler("kkb", kkb))
+
 
     updater.start_polling()
 
